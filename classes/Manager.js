2ef572c25bc10f3f8548074bddbe5c5d7bcce3de
crystalchavez99 <chavezcrystal1999@yahoo.com>
2023-11-08 12:33:30 -0800

    finished manager

--- classes/Manager.js
+++ classes/Manager.js
@@ -1,7 +1,19 @@
-class Manager {
+const {Employees} = require('./Employees.js');
+class Manager extends Employees{
+    #employeesManaged
+    constructor(name,position, salary, isHired,department,employeesManaged){
+        super(name,position,salary,isHired)
+        this.department = department;
+        this.#employeesManaged = [];
+    }
 
+    getEmployeesManaged(){
+        return this.#employeesManaged;
+    }
 
-
+    setEmployeesManaged(employee){
+        this.#employeesManaged.push(employee)
+    }
     
 }
 
