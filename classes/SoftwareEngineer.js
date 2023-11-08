8d585ac698ace869922fbbf1b40e7b06181f465a
SnageTheSnakeMage <101869780+SnageTheSnakeMage@users.noreply.github.com>
2023-11-08 13:30:20 -0800

    completed SoftwareEngingeer

--- classes/SoftwareEngineer.js
+++ classes/SoftwareEngineer.js
@@ -1,8 +1,20 @@
-class SoftwareEngineer {
+const { Employees } = require('./Employees.js');
+class SoftwareEngineer extends Employees{
 
+    #programmingLanguages
+    constructor(name,position,salary,programmingLanguages) {
+        super(name, position, salary)
+        this.#programmingLanguages = programmingLanguages;
+    }
 
+    getProgrammingLanguages() {
+        return this.#programmingLanguages
+    }
 
-    
+    setProgrammingLanguages(languages) {
+        this.#programmingLanguages.push(languages)
+    }
+
 }
 
 module.exports = {
